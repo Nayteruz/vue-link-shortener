b4e6691cbb23924458147acc389462900c4bf970
Aleksandr Pankov <nayteruz@gmail.com>
2022-07-27 16:15:37 +0700

    Мелкие изменения

--- src/stores/shortLinkStore.ts
+++ src/stores/shortLinkStore.ts
@@ -7,17 +7,15 @@ export const useShortStore = defineStore({
         isLoading: false
     }),
     getters: {
-        //doubleCount: (state) => state.counter * 2,
         getLinks: (state) => {
             if (localStorage.getItem('short_links')) {
-                // @ts-ignore
                 state.links = JSON.parse(localStorage.getItem('short_links'));
             }
             return state.links;
         },
     },
     actions: {
-        setLinks(link) {
+        setLinks(link: object) {
             link._id = Date.now();
             link.copied = false;
             this.links.push(link);
